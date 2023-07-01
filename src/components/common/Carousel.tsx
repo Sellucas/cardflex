16afe42eb4dc47d03933f069271b19a95e65b27b
Sellucas <lucassellmachado@gmail.com>
2023-07-01 14:56:03 -0300

    feat: testimonials

---
+++ src/components/common/Carousel.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { RxDotFilled } from "react-icons/rx";
+
+function Carousel() {
+  const slides = [
+    {
+      url: "https://plus.unsplash.com/premium_photo-1680792152173-42a4572e3377?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=375&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1578670957056-d47e6e5d70bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1639133694967-640f255f10fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+    },
+    {
+      url: "https://plus.unsplash.com/premium_photo-1670509045675-af9f249b1bbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=835&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1589519160732-57fc498494f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: React.SetStateAction<number>) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  return (
+    <div className="max-w-[450px] h-[600px] w-full m-auto py-8 px-4 relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <Image
+          width={50}
+          height={1}
+          src={"/left-arrow.svg"}
+          alt="right arrow"
+          onClick={prevSlide}
+        />
+      </div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <Image
+          width={50}
+          height={1}
+          src={"/right-arrow.svg"}
+          alt="right arrow"
+          onClick={nextSlide}
+        />
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            <Image width={30} height={1} src={"/dot.svg"} alt="dot" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
--- src/components/homeComponents/Testimonials.tsx
+++ src/components/homeComponents/Testimonials.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React from "react";
+import Carousel from "../common/Carousel";
+
+const whyToUse = [
+  "Verificações de identidade",
+  "Tokenização segura de dados de cartão de crédito",
+  "Pagamentos online e móveis",
+  "Regulamentações e conformidade globais",
+];
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section className="grid grid-cols-1 justify-items-center gap-8 w-full max-w-7xl p-12 relative mt-24 md:grid-cols-2">
+      <div className="w-full h-auto order-1 sm:-order-1">
+        <Carousel />
+      </div>
+      <div className="flex flex-col gap-6 justify-around items-start">
+        <h2 className="text-gray-800 text-4xl font-medium xl:text-5xl">
+          Cartão desenvolvido para empresas e profissionais
+        </h2>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          dolore rerum voluptatem enim illo quasi, reprehenderit odit beatae
+          repellat at, earum laborum provident atque eligendi facilis
+          necessitatibus id sit deserunt?
+        </p>
+        <div className="flex flex-row gap-24">
+          <article className="flex flex-col gap-4">
+            <h3 className="font-bold text-gray-800">Lojas físicas</h3>
+            <ul className="flex flex-col gap-2">
+              {whyToUse.map((item) => (
+                <li className="flex flex-row justify-start items-center text-gray-500">
+                  <span className="mr-2">
+                    <Image
+                      src={"/checked.svg"}
+                      alt="checked"
+                      width={20}
+                      height={20}
+                    />
+                  </span>{" "}
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
